fe99acd6d5d6533fc2334a9915bf3b3a6a6c8912
JJop99 <jacopo.jop@gmail.com>
2024-08-20 17:21:39 +0200

    feat: analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from "../components/Layout/Layout";
 import "../styles/globals.sass";
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics/>
     </Layout>
   );
 }
