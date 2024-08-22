93cf191fc23a845ab551415b478eee3334e7c124
JJop99 <jacopo.jop@gmail.com>
2024-08-22 11:14:59 +0200

    fix: modal swiper size

--- components/UI/Carousel.js
+++ components/UI/Carousel.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import classes from "./Carousel.module.sass";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -11,9 +12,22 @@ import "swiper/css/navigation";
 // import required modules
 import { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
 import Image from "next/image";
+import ReactModal from "react-modal";
 
 
 export default function Carousel(props) {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openModal = (image) => {
+    setSelectedImage(image);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
     
   return (
     <>
@@ -33,26 +47,80 @@ export default function Carousel(props) {
         }}
         navigation={true}
         modules={[EffectFade, Autoplay,Pagination, Navigation]}
+        className={classes.swiper}
       >
         {props.images.map(
           (image) => (
             (
-              <SwiperSlide key={image.key}>
-                <div className="object-fill w-full ">
+              <SwiperSlide key={image.key} >
+                <div className={classes.swiperSlide}>
                   <Image 
                     alt={image.image}
                     key={image.key} 
                     src={"/"+image.image} 
-                    width={800}  // Aspect ratio width
-                    height={600}
+                    width={0}
+                    height={0}
+                    sizes="100vw"
                     priority
+                    onClick={() => openModal("/" + image.image)}
+                    className={classes.swiperImage}
                   />
                 </div>
               </SwiperSlide>
+
+              
             )
           )
         )}
       </Swiper>
+
+      <ReactModal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        className={classes.modal}
+        overlayClassName={classes.overlay}
+        ariaHideApp={false}
+      >
+        <Swiper
+        autoHeight={true}
+        loop={true}
+        spaceBetween={30}
+        effect={"fade"}
+        centeredSlides={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        navigation={true}
+        modules={[EffectFade, Autoplay,Pagination, Navigation]}
+        className={classes.swiper}
+      >
+        {props.images.map(
+          (image) => (
+            (
+              <SwiperSlide key={image.key} >
+                <div className="relative w-full max-h-[90vh] overflow-y-auto" >
+                  <img 
+                    alt={image.image}
+                    key={image.key} 
+                    src={"/"+image.image} 
+                    priority
+                    onClick={() => openModal("/" + image.image)}
+                    className={classes.modalImage}
+                  />
+                </div>
+              </SwiperSlide>
+
+              
+            )
+          )
+        )}
+        </Swiper>
+      </ReactModal>
     </>
   );
 }
